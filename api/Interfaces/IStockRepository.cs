20a7df0c85e7f64593d7bd2963c91f6d39714644
Clare Wan <lisanwan710@gmail.com>
2024-11-20 16:46:36 +0800

    feat: add create portfolio api

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -11,10 +11,11 @@ namespace api.Interfaces
     public interface IStockRepository
     {
         Task<List<Stock>> GetAllAsync(QueryObject query);
-        Task<Stock?>GetByIdAsync(int id);
-        Task<Stock>CreateAsync(Stock stockModel);
-        Task<Stock?>UpdateAsync(int id, UpdateStockRequestDto stockDto);
-        Task<Stock?>DeleteAsync(int id);
+        Task<Stock?> GetByIdAsync(int id);
+        Task<Stock?> GetBySymbolAsync(string symbol);
+        Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
+        Task<Stock?> DeleteAsync(int id);
         Task<bool> StockExists(int id);
     }
 }
